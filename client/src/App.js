3fd76657c3f58a5c06cf47ef9783b9f70be5b5ce
Roaa Alghamdi <roalghamdi@tahakom.com>
2023-08-13 01:13:24 +0300

    add socket.io to the frontend and add some routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Login from "./components/Login/Login";
+import Chat from "./components/ChatForm/ChatForm";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { io } from "socket.io-client";
+const SERVER = "http://localhost:5000";
 
 function App() {
+  var socket = io(SERVER);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login socket={socket} />} />
+        <Route path="/chat" element={<Chat socket={socket} />} />
+
+        {/* TODO: Add a custom styled page for path not found error */}
+        <Route path="*" element={<h1>404 Page not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
