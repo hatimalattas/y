a52cac8f4b0d4bd105744b7f6e6813f60e4be488
Roaa Alghamdi <roalghamdi@tahakom.com>
2023-08-14 08:26:29 +0300

    setup CORS

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-
+const express = require("express");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
 
+const io = require("socket.io")(server, {
+  cors: { origin: "*" },
+});
 const PORT = process.env.PORT || 5000;
 
+// io.on("connection", (socket) => {
+//   console.log("new client connected ", socket.id);
+//   socket.emit("connection", null);
+// });
+
+io.on("connection", (socket) => {
+  io.to(socket.id).emit("socket_id", socket.id);
+
+  socket.on("send_message", (messageData) => {
+    socket.broadcast.emit("receive_message", messageData);
+  });
+});
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
