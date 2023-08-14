8d0a53d64ff3fe1efe824972ddabef73c57ef714
Roaa Alghamdi <roalghamdi@tahakom.com>
2023-08-14 09:23:33 +0300

    save users in a list

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const io = require("socket.io")(server, {
   cors: { origin: "*" },
 });
 const PORT = process.env.PORT || 5000;
+let users = [];
 
 // io.on("connection", (socket) => {
 //   console.log("new client connected ", socket.id);
@@ -16,8 +17,20 @@ const PORT = process.env.PORT || 5000;
 io.on("connection", (socket) => {
   io.to(socket.id).emit("socket_id", socket.id);
 
+  socket.on("newUser", (data) => {
+    users.push(data);
+    console.log("users ARE==== ", users);
+    socket.emit("newUserResponse", users);
+  });
+
   socket.on("send_message", (messageData) => {
     socket.broadcast.emit("receive_message", messageData);
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter((user) => user.socketID !== socket.id);
+    socket.emit("newUserResponse", users);
+    socket.disconnect();
   });
 });
 server.listen(PORT, () => {
