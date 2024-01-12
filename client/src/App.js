7515894eca10ee7985a468f130810392d065d437
Hatim Alattas <hatimalattas@Hatims-MacBook-Pro.local>
2024-01-13 02:06:19 +0300

    remove typo

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
 
-const socket = socketIO('http://localhost:4000');لهف
+const socket = socketIO('http://localhost:4000');
 
 function App() {
   return (
