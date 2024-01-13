f649eec3305c0aed952ce7cc1c216948b00c2c34
Roaa Alghamdi <roaaalghamdi95@gmail.com>
2024-01-13 22:57:35 +0300

    Update page.tsx
    remove unused lines

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-// import Image from 'next/image'
-// import styles from './page.module.css'
-
-// export default function Home() {
-//   return (
-//     <>
-// <h1>RORO</h1>
-// <a href="/api/auth/login">Login</a>
-// </>
-
-//   )
-// }
-
 
 'use client'
 import React from "react";
